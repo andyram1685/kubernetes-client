5e39204f05d6a79fc4c740d1812b7d5287e5800e
Anandaraman <anandaraman85@gmail.com>
2018-01-14 16:54:21 +0800

    Storage Class Support

---
+++ kubernetes-client/src/main/java/io/fabric8/kubernetes/client/handlers/StorageClassHandler.java
@@ -0,0 +1,17 @@
+/*
+
+
+Code space for the Handler
+
+import packages and Java Classes
+
+package io.fabric8.kubernetes.client.handlers;
+
+
+Psuedo code :
+
+public StorageClass <action> (options) {
+
+}
+
+*/

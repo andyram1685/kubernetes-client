fbc0181df14119afec22dc318fc58b5637549927
Anandaraman <anandaraman85@gmail.com>
2018-01-14 16:45:12 +0800

    K8 Storage Class support

---
+++ kubernetes-client/src/main/java/io/fabric8/kubernetes/client/dsl/internal/StorageClassOperationsImpl.java
@@ -0,0 +1,17 @@
+/ * Pesudo code for storage Class implementation.
+
+import packages and Java Classes 
+
+package io.fabric8.kubernetes.client.dsl.internal;
+
+Psuedo code :
+
+public class StorageClassOperationsImpl
+
+public StorageClassOperationsImpl() {
+
+}
+
+@Override
+
+*/
